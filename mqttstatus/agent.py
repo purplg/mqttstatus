79fa667ce2bb80aa38ca69fd885d1169023d7a76
Ben Whitley <dev@purplg.com>
2022-03-28 14:50:43 -0400

    Moved TimerLoop to its own file

--- mqttstatus/agent.py
+++ mqttstatus/agent.py
@@ -2,39 +2,11 @@ import datetime
 import os
 import json
 import subprocess
-import threading
 import psutil
 
 import paho.mqtt.client as mqtt
 
-
-class TimerLoop():
-    """
-    Calls the `handler` function ever `interval` seconds
-    """
-
-    def __init__(self, interval, handler):
-        self.interval = interval
-        self.handler = handler
-        self.thread = threading.Timer(self.interval, self._tick)
-
-    def _tick(self):
-        self.handler()
-        self.thread = threading.Timer(self.interval, self._tick)
-        self.thread.start()
-
-    def start(self):
-        """
-        Start the loop
-        """
-        self.handler()
-        self.thread.start()
-
-    def cancel(self):
-        """
-        Cancel the loop
-        """
-        self.thread.cancel()
+from mqttstatus.loop import TimerLoop
 
 
 class MQTTAgent():
---
+++ mqttstatus/loop.py
@@ -0,0 +1,30 @@
+import threading
+
+
+class TimerLoop():
+    """
+    Calls the `handler` function ever `interval` seconds
+    """
+
+    def __init__(self, interval, handler):
+        self.interval = interval
+        self.handler = handler
+        self.thread = threading.Timer(self.interval, self._tick)
+
+    def _tick(self):
+        self.handler()
+        self.thread = threading.Timer(self.interval, self._tick)
+        self.thread.start()
+
+    def start(self):
+        """
+        Start the loop
+        """
+        self.handler()
+        self.thread.start()
+
+    def cancel(self):
+        """
+        Cancel the loop
+        """
+        self.thread.cancel()
